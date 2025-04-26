4b0ccd3977f31262aee65b989779750bc2f0f226
pegaislethal <ghimirerohit21@gmail.com>
2025-04-26 16:44:32 +0545

    (update)- zod validation added for password/name/email validation

--- expense-tracker-backend/route/user.route.js
+++ expense-tracker-backend/route/user.route.js
@@ -7,17 +7,14 @@ const {
   getUserById,
   requestPasswordReset,
   resetPassword,
-  // updateProfile,
-  // getProfile,
+  
 } = require("../controller/user.controller");
-const {currentUser} = require("../controller/decodeToken.controller");
-
-const verifyToken = require("../middlewares/auth.middleware");
+const { validate } = require("../middlewares/validation.middleware");
+const { signupSchema } = require("../validations/ZodValidation.user");
+const { currentUser } = require("../controller/decodeToken.controller");
+// const verifyToken = require("../middlewares/auth.middleware");
 const authenticateUser = require("../middlewares/auth.middleware");
 // const { profileUpload } = require("../middlewares/fileUpload.middleware");
-
-
-
 
 const router = express.Router();
 
@@ -25,29 +22,49 @@ const router = express.Router();
 
 router.post(
   "/register",
+  validate(signupSchema),
   registerUser
 );
-router.post("/login", loginUser);
-router.post("/verify-otp", verifyOTPUser);
+
+router.post(
+  "/login",
+  loginUser
+);
+
+router.post(
+  "/verify-otp",
+  verifyOTPUser
+);
+
 router.get(
   "/",
   authenticateUser,
   getAllUser
 );
+
 router.get(
   "/:id",
   currentUser,
   getUserById
 );
+
 // router.get("/current-user", verifyToken);
-router.post("/password-reset/request", requestPasswordReset);  // Request password reset (send OTP/link)
-router.post("/password-reset/reset", resetPassword);  // Reset password with token/OTP
+
+router.post(
+  "/password-reset/request",
+  requestPasswordReset
+);  // Request password reset (send OTP/link)
+
+router.post(
+  "/password-reset/reset",
+  resetPassword
+);  // Reset password with token/OTP
+
 // router.patch(
 //   "/update/:id",
 //   verifyToken,
 //   updateProfile, 
 //   //  profileUpload.single("profilePicture"),
 // );
-
 
 module.exports = router;
