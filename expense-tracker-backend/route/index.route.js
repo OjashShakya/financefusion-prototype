579e3f10cfa79f3bdb702ba37e54ce941d4d8dfd
pegaislethal <ghimirerohit21@gmail.com>
2025-05-10 10:46:20 +0545

    (Update)- routing fixed for profile

--- expense-tracker-backend/route/index.route.js
+++ expense-tracker-backend/route/index.route.js
@@ -7,6 +7,7 @@ const userRoutes = require("./user.route");
 const incomeRoutes = require("./income.route");
 const savingsRoutes = require("./savings.route");
 const budgetRoutes = require("./budget.route");
+const profileRoutes= require("./profile.route")
 const verifyToken = require("../middlewares/auth.middleware");
 
 const { currentUser } = require("../controller/decodeToken.controller");
@@ -16,6 +17,7 @@ router.use("/dashboard", dashboardRoutes);
 router.use("/dashboard", budgetRoutes);
 router.use("/dashboard", incomeRoutes);
 router.use("/dashboard", savingsRoutes);
+router.use("/profile",profileRoutes)
 router.get("/current-user", verifyToken, currentUser);
 
 module.exports = router;
